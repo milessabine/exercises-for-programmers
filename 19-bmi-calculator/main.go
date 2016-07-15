2600907ce89042b529f68d09a535d90223a972b5
milessabine <miles.sabine13@houghton.edu>
2016-07-14 20:46:10 -0400

    BMI Calculator

---
+++ 19-bmi-calculator/main.go
@@ -0,0 +1,32 @@
+/*Prompt for weight and height.
+Your BMI is 19.5.
+You are within the ideal weight range.
+Or
+Your BMI is 32.5.
+You are overweight. You should see your doctor.
+bmi= (weight / height *(height * height)) *703 */
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/milessabine/prompt"
+)
+
+func main() {
+	p := prompt.New()
+
+	w := p.ScanFloat64("What is your weight in lbs?")
+	h := p.ScanFloat64("What is your height inches?")
+
+	bmi := (w / (h * h)) * 703
+	fmt.Printf("Your BMI is %0.1f\n", bmi)
+	if bmi < 18.5 {
+		fmt.Println("You are underweight. You should see your doctor.")
+	} else if bmi >= 18.5 && bmi <= 25 {
+		fmt.Println("You are within the ideal weight range")
+	} else if bmi > 25 {
+		fmt.Println("You are overweight. You should see your doctor.")
+	}
+}
