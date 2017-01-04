3822e53788d34b4515805170f10be5628bb432e2
milessabine <miles.sabine13@houghton.edu>
2017-01-03 21:04:19 -0500

    Flickr Photo
    Added form

--- 49-flickr-photo-search/main.go
+++ 49-flickr-photo-search/main.go
@@ -8,10 +8,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"html"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -28,22 +31,6 @@ type Media struct {
 
 func main() {
 
-	resp, err := http.Get("https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=nature")
-	if err != nil {
-		log.Fatal("Unable to reach URL.")
-	}
-	defer resp.Body.Close()
-
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal("Unable to read response body.")
-	}
-	t := strings.TrimPrefix(string(b), "jsonFlickrFeed(")
-	t = strings.TrimSuffix(t, ")")
-	var a Base
-	if err := json.Unmarshal([]byte(t), &a); err != nil {
-		log.Fatal("Unable to decode data.", err)
-	}
 	//for _, item := range a.Items {
 	//		fmt.Println(item.Media.URL)
 	//}
@@ -51,6 +38,10 @@ func main() {
 	const flickr = `
   <!doctype html>
   <body>
+  <form action="/search" method="post">
+    <input type="text" name="tags" />
+    <button type="submit"> submit </button>
+  </form>
   {{range .Items}}
   <img src="{{.Media.URL}}">
   {{end}}
@@ -60,10 +51,44 @@ func main() {
 	templ := template.Must(template.New("xyz").Parse(flickr))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err := templ.Execute(w, a)
+		var t Base
+		if r.Method == "POST" {
+			tags := r.FormValue("tags")
+			var err error
+			t, err = getFlickrData(tags)
+			if err != nil {
+				log.Println("Error getting Flickr data", err)
+				return
+			}
+		}
+
+		err := templ.Execute(w, t)
 		if err != nil {
 			log.Println("executing template:", err)
 		}
 	})
 	http.ListenAndServe(":8080", nil)
+}
+func getFlickrData(tags string) (Base, error) {
+	v := url.Values{}
+	v.Set("format", "json")
+	v.Set("tags", tags)
+	resp, err := http.Get("https://api.flickr.com/services/feeds/photos_public.gne?" + v.Encode())
+	if err != nil {
+		return Base{}, fmt.Errorf("Error getting URL: %s", err)
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return Base{}, fmt.Errorf("Error reading response body: %s", err)
+	}
+	body := html.UnescapeString(string(b))
+	t := strings.TrimPrefix(body, "jsonFlickrFeed(")
+	t = strings.TrimSuffix(t, ")")
+	var a Base
+	if err := json.Unmarshal([]byte(t), &a); err != nil {
+		return Base{}, fmt.Errorf("Error decoding json: %s", err)
+	}
+	return a, nil
 }
